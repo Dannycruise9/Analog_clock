223ca5178ddacc30727421a3f9f16311ed8399c5
Dannycruise9 <125276298+Dannycruise9@users.noreply.github.com>
2023-04-14 19:12:44 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "analog-clock-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/main.js",
+  "/images/icon-72x72.png",
+  "/images/icon-96x96.png",
+  "/images/icon-128x128.png",
+  "/images/icon-144x144.png",
+  "/images/icon-152x152.png",
+  "/images/icon-192x192.png",
+  "/images/icon-384x384.png",
+  "/images/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
---
+++ main.js
@@ -0,0 +1,117 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then((reg) => console.log("Service Worker registered"))
+      .catch((err) => console.log("Service Worker not registered", err));
+  });
+}
+
+// progress web app
+const CACHE_NAME = "analog-clock-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/main.js",
+  "/icons/icon-512.png",
+  "/icons/maskable_icon_x512 (1).png",
+  "/icons/icon-1024.png",
+ 
+
+
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// clock and alarm
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+const hour = document.querySelector('.hour');
+const setAlarmButton = document.getElementById("set-alarm");
+const clearAlarmButton = document.getElementById("clear-alarm");
+const tickingSound = new Audio("/sound/ticking-clock_1-27477.mp3");
+const alarmAudio = new Audio("/sound/Alarm-Fast-A1-www.fesliyanstudios.com.mp3");
+const alarmHourInput = document.querySelector('#alarm-hour');
+const alarmMinInput = document.querySelector('#alarm-min');
+const amPmInput = document.querySelector('#am-pm');
+
+let alarmHour, alarmMin, amPm;
+
+setInterval (() =>{
+    const time = new Date();
+    const secDeg = time.getSeconds()/ 60 * 360 -90;
+    const hourDeg = time.getHours()/ 12 * 360 -90;
+    const minDeg = time.getMinutes()/ 60 * 360 -90;
+    sec.style.transform = `rotate(${secDeg}deg)`;
+    min.style.transform = `rotate(${minDeg}deg)`;
+    hour.style.transform = `rotate(${hourDeg}deg)`;
+    checkAlarm(time); // check if alarm should go off
+    tickingSound.play(); // play ticking sound every second
+},1000);
+
+const alarmForm = document.querySelector('#alarm-form');
+
+alarmForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  setAlarm();
+});
+
+
+
+function setAlarm() {
+    alarmHour = parseInt(alarmHourInput.value);
+    alarmMin = parseInt(alarmMinInput.value);
+    amPm = amPmInput.value;
+    
+    if (amPm === 'pm' && alarmHour !== 12) {
+        alarmHour += 12;
+    } else if (amPm === 'am' && alarmHour === 12) {
+        alarmHour = 0;
+    }
+}
+
+function checkAlarm(time) {
+    if (time.getHours() === alarmHour && time.getMinutes() === alarmMin && time.getSeconds() === 0) {
+      return  alarmAudio.play();
+    }
+}
+
+setAlarmButton.addEventListener('click', setAlarm);
+clearAlarmButton.addEventListener('click', () => {
+    alarmHourInput.value = '';
+    alarmMinInput.value = '';
+    amPmInput.value = 'am';
+    alarmHour = null;
+    alarmMin = null;
+    amPm = null;
+});
